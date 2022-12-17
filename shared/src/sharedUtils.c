a08fb9a0ad19aca2d8abe3fcd1463a7ac2ab3ed4
LeoGuido99 <lcorrales@frba.utn.edu.ar>
2022-12-17 03:49:08 +0000

    Cambio el crear conexion

--- shared/src/sharedUtils.c
+++ shared/src/sharedUtils.c
@@ -196,13 +196,10 @@ int esperar_cliente(int socket_servidor, char* cliente) {
 	return socket_cliente;
 }
 
-
 int crear_conexion(char *ip, char* puerto)
 {
 	struct addrinfo hints;
 	struct addrinfo *server_info;
-
-	struct addrinfo *clientInfo, *p;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_UNSPEC;
@@ -211,50 +208,91 @@ int crear_conexion(char *ip, char* puerto)
 
 	getaddrinfo(ip, puerto, &hints, &server_info);
 
-	int socket_cliente;
-
+	// Crear el socket
+	int socket_cliente = socket(server_info->ai_family,
+								server_info->ai_socktype,
+								server_info->ai_protocol);
 
 	// Fallo al crear el socket
 	if (socket_cliente == -1) {
-		// Pasar logger correspondiente por parametro
 		log_error(logger, "Hubo un error al crear el socket del servidor");
 		return 0;
 	}
 
 	// Conectar el socket...
 	// Error al conectar
-
-	for(p = server_info; p != NULL; p = p->ai_next){
-		// Crear el socket
-		socket_cliente = socket(server_info->ai_family,
-									server_info->ai_socktype,
-									server_info->ai_protocol);
-		if(socket_cliente == -1)
-			continue;
-
-		if(connect(socket_cliente, p->ai_addr, p->ai_addrlen) != -1)
-			break;
-
-		close(socket_cliente);
-
-//	if(connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) == -1) {
-//		log_error(logger, "Error al conectar al servidor");
-//		freeaddrinfo(server_info);
-//		return -1;
-//	}
-	}
-	if(socket_cliente != -1 && p != NULL){
-		log_info(logger,"Conexion exitosa!!!!!!!!!!!!!!!!!!!");
-		return socket_cliente;
-	}else{
-		perror("socket cliente fallido");
+	if(connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) == -1) {
+		log_error(logger, "Error al conectar al servidor");
+		freeaddrinfo(server_info);
+		return -1;
 	}
 
-	//freeaddrinfo(server_info);
+	freeaddrinfo(server_info);
 
-	return -1;
-
+	return socket_cliente;
 }
+
+
+
+
+//int crear_conexion(char *ip, char* puerto)
+//{
+//	struct addrinfo hints;
+//	struct addrinfo *server_info;
+//
+//	struct addrinfo *clientInfo, *p;
+//
+//	memset(&hints, 0, sizeof(hints));
+//	hints.ai_family = AF_UNSPEC;
+//	hints.ai_socktype = SOCK_STREAM;
+//	hints.ai_flags = AI_PASSIVE;
+//
+//	getaddrinfo(ip, puerto, &hints, &server_info);
+//
+//	int socket_cliente;
+//
+//
+//	// Fallo al crear el socket
+//	if (socket_cliente == -1) {
+//		// Pasar logger correspondiente por parametro
+//		log_error(logger, "Hubo un error al crear el socket del servidor");
+//		return 0;
+//	}
+//
+//	// Conectar el socket...
+//	// Error al conectar
+//
+//	for(p = server_info; p != NULL; p = p->ai_next){
+//		// Crear el socket
+//		socket_cliente = socket(server_info->ai_family,
+//									server_info->ai_socktype,
+//									server_info->ai_protocol);
+//		if(socket_cliente == -1)
+//			continue;
+//
+//		if(connect(socket_cliente, p->ai_addr, p->ai_addrlen) != -1)
+//			break;
+//
+//		close(socket_cliente);
+//
+////	if(connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen) == -1) {
+////		log_error(logger, "Error al conectar al servidor");
+////		freeaddrinfo(server_info);
+////		return -1;
+////	}
+//	}
+//	if(socket_cliente != -1 && p != NULL){
+//		log_info(logger,"Conexion exitosa!!!!!!!!!!!!!!!!!!!");
+//		return socket_cliente;
+//	}else{
+//		perror("socket cliente fallido");
+//	}
+//
+//	//freeaddrinfo(server_info);
+//
+//	return -1;
+//
+//}
 
 
 void enviar_mensaje(char* mensaje, int socket_cliente, int cod_op) //podríamos usar esto polimorficamente con cualquier mensaje para no
